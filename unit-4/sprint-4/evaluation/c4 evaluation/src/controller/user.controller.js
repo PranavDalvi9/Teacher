654405dfc7e9218bea24e562f7053c3b86b6a104
PranavDalvi9 <pranavdalvi9@gmail.com>
2022-02-21 15:59:30 +0530

    second

--- unit-4/sprint-4/evaluation/c4 evaluation/src/controller/user.controller.js
+++ unit-4/sprint-4/evaluation/c4 evaluation/src/controller/user.controller.js
@@ -1,11 +1,51 @@
+const {body , validationResult} = require("express-validator");
+
 const express = require("express");
 const User = require("../models/user.model");
 // const user = require("../models/user.model")
 const router = express.Router();
 
-router.get("", async(req,res) =>{
+router.post("", 
+body("first_name")
+.isString()
+.isLowercase()
+.isLength({min:3 , max:30})
+.withMessage("first name should be between 3 to 30 chars"),
+
+body("last_name")
+.isString()
+.isLowercase()
+.isLength({min:3 , max:30})
+.withMessage("last name should be between 3 to 30 chars"),
+
+body("age")
+.isAlphanumeric(),
+
+body("email")
+.isEmail()
+.custom(async (value) => {
+    const user = await User.findOne({email : value});
+
+    if(user){
+        throw new Error("email is in use")
+    }
+    return true;
+}),
+
+async(req,res) =>{
     try {
-        const user = await User.find().lean().exec();
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            let newErrors;
+            newErrors = errors.array().map((err) => {
+                console.log("err", err);
+                return{key:err.param, message:err.message}
+            })
+            return res.status(400).send({errors: newErrors})
+        }
+
+       
+        const user = await User.create(req.body);
         return res.send(user);
     } catch (error) {
         console.log(error.message);
--- unit-4/sprint-4/evaluation/c4 evaluation/src/models/user.model.js
+++ unit-4/sprint-4/evaluation/c4 evaluation/src/models/user.model.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     first_name : {type: String , require:true},
-    last_name : {type:String,  require:true}
+    last_name : {type:String,  require:true},
+    age :{type:Number,  require:true},
+    email:{type:String,  require:true}
 },{
     versionKey:false,
     timestamps:true
