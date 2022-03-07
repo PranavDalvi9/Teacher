2c3c7e1b431b38cc3c5803e3a05e9a5e315e4b35
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-03-07 10:24:09 +0530

    evaluation 1

--- unit-5/sprint-1/evaluation/pranav-c1/src/App.jsx
+++ unit-5/sprint-1/evaluation/pranav-c1/src/App.jsx
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
-import {useState} from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   // const data = ["Books", "Pens", "Notebooks",  "InkPens"]
-  const [addBook, setAddBook] = useState(13)
+  const [addBook, setAddBook] = useState(13);
+  const [addPen, setPen] = useState(10);
+  const [addNotebook, setNotebook] = useState(44);
+  const [addInkpen, setAddInkpen] = useState(78);
   return (
-  <div className="items">
+    <>
+      <div className="items">
+        <span>Books:</span>
 
-    <span>Books:</span>
+        <button
+          className="addBook"
+          onClick={() => {
+            setAddBook(addBook + 1);
+          }}
+        >
+          +
+        </button>
 
-    <button className="addBook" onClick={ () => {
-      setAddBook(addBook+1)
-    }}>+</button>
+        <button
+          className="remBook"
+          onClick={() => {
+            setAddBook(addBook - 1);
+          }}
+        >
+          -
+        </button>
 
-    <button className="remBook" onClick={ () => {
-      setAddBook(addBook-1)
-    }}>-</button>
+        <span className="totalBooks">{addBook}</span>
+      </div>
 
-    <span className="totalBooks">{addBook}</span>
-  </div>
+      <div className="items">
+        <span>Pens:</span>
+
+        <button
+          className="addPen"
+          onClick={() => {
+            setPen(addPen + 1);
+          }}
+        >
+          +
+        </button>
+
+        <button
+          className="remPen"
+          onClick={() => {
+            setPen(addPen - 1);
+          }}
+        >
+          -
+        </button>
+
+        <span className="totalPens">{addPen}</span>
+      </div>
+
+      <div className="items">
+        <span>Notebooks:</span>
+
+        <button
+          className="addNotebook "
+          onClick={() => {
+            setNotebook(addNotebook + 1);
+          }}
+        >
+          +
+        </button>
+
+        <button
+          className="remNotebook "
+          onClick={() => {
+            setNotebook(addNotebook - 1);
+          }}
+        >
+          -
+        </button>
+
+        <span className="totalNotebooks">{addNotebook}</span>
+      </div>
+
+      <div className="items">
+        <span>InkPens:</span>
+
+        <button
+          className="addInkpen "
+          onClick={() => {
+            setAddInkpen(addInkpen + 1);
+          }}
+        >
+          +
+        </button>
+
+        <button
+          className="remInkpen "
+          onClick={() => {
+            setAddInkpen(addInkpen - 1);
+          }}
+        >
+          -
+        </button>
+
+        <span className="totalInkpens">{addInkpen}</span>
+      </div>
+    </>
   );
 }
 
