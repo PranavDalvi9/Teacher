76dd71ac77bc2cf5f8994d6de6e1d40b21c38126
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-04-16 20:50:13 +0530

    Test

--- unit-6/sprint-2/evaluation/citycountry/src/Components/Home.jsx
+++ unit-6/sprint-2/evaluation/citycountry/src/Components/Home.jsx
@@ -1,17 +1,23 @@
 // import React from 'react'
 // import { useSelector } from 'react-redux';
 // import { useDispatch } from 'react-redux';
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 
 export default function Home() {
 
   const [data , setData] = useState([])
 
+  useEffect(() => {
+    fetch("http://localhost:8080/add-city")
+    .then((res) => res.json())
+    .then((res) => setData(res))
+  },[])
+
 
   fetch("http://localhost:8080/add-city")
 .then((res) => res.json())
 .then((res) => setData(res))
-.catch((err) => console.log(err))
+// .catch((err) => console.log(err))
 
 // console.log(data)
   return (
@@ -29,7 +35,20 @@ export default function Home() {
         </tr>
 
 
-{}
+{
+  data.map((e) => (
+    <tr>
+      {/* <td>{e.name}</td> */}
+     
+          <td>{e.id}</td>
+          <td>{e.countryr}</td>
+          <td>{e.name}</td>
+          <td>{e.population}</td>
+          <button>Edit</button>
+          <button>Delete</button>
+    </tr>
+  ))
+}
         
       </table>
     </div>
