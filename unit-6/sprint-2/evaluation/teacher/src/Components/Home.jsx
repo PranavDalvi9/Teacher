140795fed40042a8df8206e0b7457eceb8b1b6ab
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-04-17 19:00:46 +0530

    Teacher

--- unit-6/sprint-2/evaluation/teacher/src/Components/Home.jsx
+++ unit-6/sprint-2/evaluation/teacher/src/Components/Home.jsx
@@ -5,40 +5,45 @@ import "./Home.css"
 export default function Home() {
 
 
-  const [data , setData] = useState([])
-  
-  useEffect(()=> {
-    axios.get('http://localhost:2344/teacher').then((res)=> setData(res.data));
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:2344/teacher').then((res) => setData(res.data));
     // console.log("data1",data[0].age)
-  },[])
-  console.log("data",data)
+  }, [])
+  console.log("data", data)
 
   return (
-    <div>
+    <div >
       <h1>Home</h1>
 
-{
-  data.map((e) => (
-    <div className='individualTec'>
-    <h3>Name : {e.name}</h3>
-    <h3>Age : {e.age}</h3>
-    <h3>Gender : {e.gender}</h3>
-    <h3>Classes :</h3>
-<div>
-{
-      e.classes.map((el) => (
-        <p>{el.class1}</p>
-      ))
-    } 
-</div>
-    <div>
 
-    </div>
-    
-    </div>
-  ))
-}
+      <div className='individualTec'>
+      {
+        data.map((e) => (
+
+
+          <div className='IndiviTeachDat' >
+            <h3>Name : {e.name}</h3>
+            <h3>Age : {e.age}</h3>
+            <h3>Gender : {e.gender}</h3>
+            
+            <div className='classFlex'>
+            <h3>Classes :</h3>
+              {
+                e.classes.map((el) => (
+                  <p>{el.class1}</p>
+                ))
+              }
+            </div>
+
+          </div>
+        ))
+      }
+
+      </div>
       
+
     </div>
   )
 }
