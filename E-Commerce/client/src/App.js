f391df44f8ac4c63a5627779362a5f74ebba0650
PranavDalvi9 <96105012+PranavDalvi9@users.noreply.github.com>
2022-05-09 23:34:21 +0530

    Landing Page Created

--- E-Commerce/client/src/App.js
+++ E-Commerce/client/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/LandingPage/LandingPage';
+import Category from './Components/CategoryOfProducts/Category';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <LandingPage/>
+      <Category/>
     </div>
   );
 }
---
+++ E-Commerce/client/src/Components/CategoryOfProducts/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Category.css"
+
+export default function Category() {
+  return (
+    <div className='Category'>
+      <div><p className='CategoryName'>Men's Fashion</p><p className='CategoryShopNow'>Shop Now</p></div>
+      <div><p className='CategoryName'>Women's Fashion</p><p className='CategoryShopNow'>Shop Now</p></div>
+      <div><p className='CategoryName'>Babay Fashion</p><p className='CategoryShopNow'>Shop Now</p></div>
+    </div>
+  )
+}
